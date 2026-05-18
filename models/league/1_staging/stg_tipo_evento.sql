d1fd35ad632dec9901a62c797be62107b9119b93
Judith Fernández <judithfer9@gmail.com>
2026-05-18 10:33:47 +0000

    mas actualizaciones

--- models/league/1_staging/stg_tipo_evento.sql
+++ models/league/1_staging/stg_tipo_evento.sql
@@ -1,17 +1,20 @@
 with source as (
-
-    select * from {{ ref('base__eventos') }}
-
+    -- 1. Lo cogemos de la source de eventos real de forma dinámica
+    select distinct tipo_evento 
+    from {{ ref('base__eventos') }}
+    where tipo_evento is not null
 ),
 
 final as (
-
-    select distinct
-        {{ dbt_utils.generate_surrogate_key(['tipo_evento']) }}  as id,
-        tipo_evento                                              as nombre
+    select
+        -- Generamos un ID secuencial único y limpio para cada tipo de evento
+        row_number() over (order by tipo_evento) as id_tipo_evento,
+        tipo_evento as descripcion_evento
     from source
-    where tipo_evento is not null
-
 )
 
-select * from final
+-- 2. Estructura limpia: sin columnas 'id' repetidas ni nombres ambiguos
+select 
+    id_tipo_evento,
+    descripcion_evento
+from final
